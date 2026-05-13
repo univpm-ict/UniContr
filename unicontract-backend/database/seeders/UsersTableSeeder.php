292ca6aadfe9135d52b9ddef4de137d91594c2ee
Francesco Antonio Pileo <f.a.pileo@staff.univpm.it>
2026-05-13 11:52:25 +0200

    aggiunti utenti univpm a seeder utenti

--- unicontract-backend/database/seeders/UsersTableSeeder.php
+++ unicontract-backend/database/seeders/UsersTableSeeder.php
@@ -14,42 +14,59 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::where('email', 'enrico.oliva@uniurb.it')->first();   
-        if ($user==null) {     
+        $user = User::where('email', 'f.a.pileo@staff.univpm.it')->first();
+        if ($user==null) {
             $user = User::firstOrCreate( [
-                'email' => 'enrico.oliva@uniurb.it' ,
+                'email' => 'f.a.pileo@staff.univpm.it' ,
                 'password' => Hash::make( 'testadm1n' ) ,
-                'name' => 'Enrico Oliva' ,
-                'v_ie_ru_personale_id_ab'=> 39842,  
-                'cf' => 'LVONRC76C29L456I',
-                'nome' => 'Enrico',
-                'cognome' => 'Oliva'
-            ] );        
+                'name' => 'Francesco Antonio Pileo' ,
+                'v_ie_ru_personale_id_ab'=> 186177,
+                'cf' => 'PLIFNC91T02E885P',
+                'nome' => 'Francesco Antonio',
+                'cognome' => 'Pileo'
+            ] );
         }
         if (!$user->hasRole('super-admin')){
-            $user->assignRole('super-admin');  
+            $user->assignRole('super-admin');
         }
 
-        $user = User::where('email', 'test.admin@uniurb.it')->first();   
-        if ($user==null) {     
+        $user = User::where('email', 'd.attanasio@staff.univpm.it')->first();
+        if ($user==null) {
             $user = User::firstOrCreate( [
-                'email' => 'test.admin@uniurb.it' ,
+                'email' => 'd.attanasio@staff.univpm.it' ,
+                'password' => Hash::make( 'testadm1n' ) ,
+                'name' => 'Dante Attanasio' ,
+                'v_ie_ru_personale_id_ab'=> 195317,
+                'cf' => 'TTNDNT81P22C933C',
+                'nome' => 'Dante',
+                'cognome' => 'Attanasio'
+            ] );
+        }
+        if (!$user->hasRole('super-admin')){
+            $user->assignRole('super-admin');
+        }
+
+
+        $user = User::where('email', 'test.admin@univpm.it')->first();
+        if ($user==null) {
+            $user = User::firstOrCreate( [
+                'email' => 'test.admin@univpm.it' ,
                 'password' => Hash::make( 'testadm1n' ) ,
                 'name' => 'test admin' ,
                 'cf' => '12346789LLLLLLL',
                 'nome' => 'test',
                 'cognome' => 'admin'
                 //'v_ie_ru_personale_id_ab'=> 39842,
-            ] );        
+            ] );
         }
         if (!$user->hasRole('admin')){
-            $user->assignRole('admin');  
-        }      
+            $user->assignRole('admin');
+        }
 
-        $user = User::where('email', 'test.user@uniurb.it')->first();        
+        $user = User::where('email', 'test.user@univpm.it')->first();
         if ($user==null){
             $user = User::firstOrCreate([
-                'email' => 'test.user@uniurb.it' ,
+                'email' => 'test.user@univpm.it' ,
                 'password' => Hash::make( 'testuser' ) ,
                 'name' => 'test user' ,
                 'cf' => '12346789LLLLLLL',
