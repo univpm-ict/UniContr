f4add77fb7aece7d0aecd157bc2dd7215fbc47cd
Francesco Antonio Pileo <f.a.pileo@staff.univpm.it>
2026-05-13 10:58:09 +0200

    configurazioni idp beckend

--- unicontract-backend/app/Listeners/LoginListener.php
+++ unicontract-backend/app/Listeners/LoginListener.php
@@ -40,98 +40,98 @@ class LoginListener
     public function handle(Saml2LoginEvent $event)
     {
         $messageId = $event->getSaml2Auth()->getLastMessageId();
-        Log::info('messageId [' . $messageId . ']');   
+        Log::info('messageId [' . $messageId . ']');
 
         $attributesName = [
+            'cn' => 'urn:oid:2.5.4.3',
+            'codiceFiscale' => 'urn:oid:1.3.6.1.4.1.25178.1.2.15',
+            'eduPersonAffiliation' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.1',
             'eduPersonEntitlement' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.7',
-            'cn' => 'urn:oid:2.5.4.3',                            
-            'displayName' => 'urn:oid:2.16.840.1.113730.3.1.241',
-            'codiceFiscale' => 'urn:oid:1.3.6.1.4.1.4203.666.11.11.1.0',
-            'eduPersonOrgDN' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.3',
-            'pid' => 'urn:oid:2.5.4.10',
-            'uid' => 'urn:oid:0.9.2342.19200300.100.1.1',
-            'eduPersonScopedAffiliation' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.9',
-            'surname' => 'urn:oid:2.5.4.4',
-            'schacHomeOrganization' => 'urn:oid:1.3.6.1.4.1.25178.1.2.9',
             'eduPersonPrincipalName' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.6',
-            'realm' => 'urn:oid:2.5.4.100',
-            'eduPersonUniqueId' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.13',
+            'eduPersonScopedAffiliation' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.9',
+            'eduPersonTargetedID' => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.10',
             'email' => 'urn:oid:0.9.2342.19200300.100.1.3',
-            'matricola' => 'urn:oid:1.3.6.1.4.1.27280.1.20',    
-            'ruolo' => 'urn:oid:1.3.6.1.4.1.27280.1.13'        
+            'givenName' => 'urn:oid:2.5.4.42',
+            'matricola' => 'urn:oid:2.5.4.3',
+            'physicalDeliveryOfficeName' => 'urn:oid:2.5.4.19',
+            'ruolo' => 'ateneoRuolo',
+            'schacHomeOrganization' => 'urn:oid:1.3.6.1.4.1.25178.1.2.9',
+            'schacHomeOrganizationType' => 'urn:oid:1.3.6.1.4.1.25178.1.2.10',
+            'surname' => 'urn:oid:2.5.4.4',
+            'uid' => 'urn:oid:0.9.2342.19200300.100.1.1'
         ];
 
         $user = $event->getSaml2User();
-        Log::info('user [' . $user->getUserId() . ']');   
+        Log::info('user [' . $user->getUserId() . ']');
         $user->parseAttributes($attributesName);
-        
-        $userData = new \App\User;    
-        try{ 
+
+        $userData = new \App\User;
+        try{
             $userData->id = $user->getUserId();
             $userData->attributes = $user->getAttributes();
-            $userData->name = $user->displayName[0];
-            $userData->email = $user->email[0];
-            Log::info('email [' . $userData->email . ']');   
+            $userData->name = $user->givenName[0] . ' ' . $user->surname[0];
+            $userData->email = !empty($user->email) ? $user->email[0] : $user->eduPersonPrincipalName[0];
+            Log::info('email [' . $userData->email . ']');
             // Check if the email domain is unauthorized
-            if (strpos($userData->email, '@campus.uniurb.it') !== false) {
+            if (strpos($userData->email, '@studenti.univpm.it') !== false) {
                 // If the email domain is unauthorized, log it and return an error
                 Log::info('Unauthorized user with email: ' . $userData->email);
                 abort(401, trans('global.utente_non_autorizzato'));
             }
-            
+
             // Check if the "ruolo" attribute exists and is not empty
             if (isset($user->ruolo[0]) && !empty($user->ruolo[0])) {
-                $userData->ruolo = $user->ruolo[0];            
-                Log::info('ruolo [' . $userData->ruolo . ']');   
+                $userData->ruolo = $user->ruolo[0];
+                Log::info('ruolo [' . $userData->ruolo . ']');
             } else {
                 // If "ruolo" is missing or empty, return an error
                 throw new Exception('Ruolo non presente o vuoto');
             }
-    
+
             $userData->eduPersonScopedAffiliation = $user->eduPersonScopedAffiliation;
             $userData->password =Hash::make($user->codiceFiscale[0]);
             $userData->assertion = $user->getRawSamlAssertion();
             $userData->cf = $user->codiceFiscale[0];
-        }catch(Exception $e){            
-            Log::info('Errore metadati utente passati dall\'idp');                     
+        }catch(Exception $e){
+            Log::info('Errore metadati utente passati dall\'idp');
             $handler = new Handler(Container::getInstance());
             $handler->report($e);
-            Log::info('Utente non autorizzato: '.$userData->email.' '.$userData->ruolo);            
+            Log::info('Utente non autorizzato: '.$userData->email.' '.$userData->ruolo);
             abort(401,  trans('global.utente_non_autorizzato'));
         }
 
         //check if email already exists and fetch user
         $laravelUser = \App\User::where('email', $userData['email'])->first();
-        Log::info('laravel user [' . $laravelUser . ']');                     
-        //ulteriore verifica attraverso il codice fiscale 
+        Log::info('laravel user [' . $laravelUser . ']');
+        //ulteriore verifica attraverso il codice fiscale
         if ($laravelUser===null){
             $laravelUser = \App\User::where('cf', $userData['cf'])->first();
             if ($laravelUser !== null ){
-                //aggiornare email 
-                if (EmailHelper::hasAllowedDomain($userData['email'])){   
+                //aggiornare email
+                if (EmailHelper::hasAllowedDomain($userData['email'])){
                     $laravelUser->email = $userData['email'];
-                    $laravelUser->save();  
-                    Log::info('Aggiornata email laravel user [' . $laravelUser->name . ']'); 
-                } 
+                    $laravelUser->save();
+                    Log::info('Aggiornata email laravel user [' . $laravelUser->name . ']');
+                }
             }
         }
-                        
+
         try{
             //if email doesn't exist, create new user
             if($laravelUser === null)
-            {		
-                Log::info('inserisci utente [' . $userData->name . ' '. $userData->email . ' '.$user->codiceFiscale[0].' ]');                
-                $laravelUser = new \App\User;                             
+            {
+                Log::info('inserisci utente [' . $userData->name . ' '. $userData->email . ' '.$user->codiceFiscale[0].' ]');
+                $laravelUser = new \App\User;
                 $laravelUser->name = $userData['name'];
                 $laravelUser->email = $userData['email'];
-                $laravelUser->password = Hash::make($user->codiceFiscale[0]);          
-                //Per ulteriore controllo memorizza anche il codice fiscale       
+                $laravelUser->password = Hash::make($user->codiceFiscale[0]);
+                //Per ulteriore controllo memorizza anche il codice fiscale
                 $laravelUser->cf = $user->codiceFiscale[0];
 
-                Log::info('istanza utente [' . $laravelUser->name . ' '. $laravelUser->email . ' ]');        
+                Log::info('istanza utente [' . $laravelUser->name . ' '. $laravelUser->email . ' ]');
                 //va determinato il ruolo da assegnare 1) leggerlo da file id configurazione, 2) ruolo di default
                 $service = new LoginService();
-                Log::info('istanza service');        
+                Log::info('istanza service');
                 $data = null;
                 if ($userData['ruolo'] && Ruolo::isRuoloDocente($userData['ruolo'])){
                     $data = $service->findDocenteData($userData->email);
@@ -139,18 +139,18 @@ class LoginListener
                     $data = $service->findUserRoleAndData($userData->email);
                 }
 
-                Log::info('ruoli [' . implode(';',$data['ruoli']) . ']');     
+                Log::info('ruoli [' . implode(';',$data['ruoli']) . ']');
 
-                if ($data){            
+                if ($data){
                     $laravelUser->v_ie_ru_personale_id_ab = $data['id_ab'];
-                    $laravelUser->save();                       
-                    $laravelUser->assignRole($data['ruoli']);                                  
+                    $laravelUser->save();
+                    $laravelUser->assignRole($data['ruoli']);
                 }
-            
+
             }
-                 
+
         } catch (\Exception $e) {
-            Log::info('Errore nuovo utente non creato');                            
+            Log::info('Errore nuovo utente non creato');
             $handler = new Handler(Container::getInstance());
             $handler->report($e);
 
@@ -161,8 +161,8 @@ class LoginListener
         // Here we save the received nameId and sessionIndex needed later for the LogoutRequest
         session()->put('nameId', $user->getNameId());
         session()->put('sessionIndex', $user->getSessionIndex());
-        
-        Log::info('login [' . $laravelUser->name . ']');  
+
+        Log::info('login [' . $laravelUser->name . ']');
         Auth::login($laravelUser);
-    }   
+    }
 }
--- unicontract-backend/config/saml2/preprod_idp_settings.php
+++ unicontract-backend/config/saml2/preprod_idp_settings.php
@@ -26,7 +26,7 @@ return $settings = array(
 
     // Service Provider Data that we are deploying
     'sp' => array(
-        
+
         // Specifies constraints on the name identifier to be used to
         // represent the requested subject.
         // Take a look on lib/Saml2/Constants.php to see the NameIdFormat supported
@@ -34,8 +34,8 @@ return $settings = array(
 
         // Usually x509cert and privateKey of the SP are provided by files placed at
         // the certs folder. But we can also provide them with the following parameters
-        'x509cert' => env('SAML2_SP_x509',''),
-        'privateKey' => env('SAML2_SP_PRIVATEKEY',''),
+        'x509cert' => file_exists(env('SAML2_SP_x509_PATH', '')) ? file_get_contents(env('SAML2_SP_x509_PATH', '')) : '',
+        'privateKey' => file_exists(env('SAML2_SP_PRIVATEKEY_PATH', '')) ? file_get_contents(env('SAML2_SP_PRIVATEKEY_PATH', '')) : '',
 
         // Identifier (URI) of the SP entity.
         // Leave blank to use the 'saml_metadata' route.
@@ -56,7 +56,7 @@ return $settings = array(
             // URL Location where the <Response> from the IdP will be returned,
             // using HTTP-Redirect binding.
             // Leave blank to use the 'saml_sls' route
-            'url' => '',        
+            'url' => '',
         ),
     ),
 
@@ -64,52 +64,39 @@ return $settings = array(
     'idp' => array(
         // Identifier of the IdP entity  (must be a URI)
         //https://idptest.uniurb.it/idp/shibboleth
-        'entityId' => env('SAML2_IDP_ENTITYID', $idp_host.'idp/shibboleth'), //. 'idp/shibboleth'
+        'entityId' => env('SAML2_'.$this_idp_env_id.'_IDP_ENTITYID', $idp_host.'idp/shibboleth'), //. 'idp/shibboleth'
         // SSO endpoint info of the IdP. (Authentication Request protocol)
         'singleSignOnService' => array(
             // URL Target of the IdP where the SP will send the Authentication Request Message,
             // using HTTP-Redirect binding.
             'url' => $idp_host.'idp/profile/SAML2/Redirect/SSO',
-            
+
         ),
         // SLO endpoint info of the IdP.
         'singleLogoutService' => array(
             // URL Location of the IdP where the SP will send the SLO Request,
             // using HTTP-Redirect binding.
             //https://ds90p01.bib.uniurb.it/Shibboleth.sso/Logout?return=https://idp.uniurb.it/idp/profile/Logout
-            'url' => $idp_host.'idp/profile/Logout',
+            'url' => $idp_host.'idp/profile/SAML2/Redirect/SLO',
         ),
         // Public x509 certificate of the IdP
-        'x509cert' => env('SAML2_IDP_x509', 'MIIFhTCCA22gAwIBAgIJANN/ysx6nt9TMA0GCSqGSIb3DQEBCwUAMFgxCzAJBgNV
-        BAYTAklUMQswCQYDVQQIDAJQVTEPMA0GA1UEBwwGVXJiaW5vMQ8wDQYDVQQKDAZV
-        bml1cmIxGjAYBgNVBAMMEWlkcHRlc3QudW5pdXJiLml0MCAXDTE3MDMwOTE0NDgw
-        NloYDzIxMTcwMjEzMTQ0ODA2WjBYMQswCQYDVQQGEwJJVDELMAkGA1UECAwCUFUx
-        DzANBgNVBAcMBlVyYmlubzEPMA0GA1UECgwGVW5pdXJiMRowGAYDVQQDDBFpZHB0
-        ZXN0LnVuaXVyYi5pdDCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAMA+
-        BQsFgWbk4eUg9NtHi6p2f4NiufdMfw0g4AR1fsLw8KXbEsndheEhwzCHlIFJeg8L
-        0xvz4yn42xx6/ICyyUJBgsNv8v0dCL/gft04Jms1F7SaEfU2BNYHdZF1i08oDm96
-        YXjS5M/P7X3mxPxDV0pzuseZ3ykgdIQcuG4q3L3lbmXOwoJh5u7LrxKmS3qwBcDC
-        pQSf2OjfEZVCILANN7SXUAbW9celzD6QaM7TOL/UYHL+0dB35Q/HdNp5mxdNTEh7
-        l79qljmRXWlVFS5KeTk3M0P3C2Ni3kHj5wP9mOPEAVS5efcWlV+vd2sHQnSoMfvK
-        tiwAR9aKJ/ebfjcIHP1cLaeedJLFzcgkvgXm5bR+leOqGMLedndKjtKt8LjHqlgr
-        GjJ8ICg86rlkhSRPbH1gq61LunzYhGn4AuQYNJYRnqTXu9U4kf2djYIQmezdVGMp
-        lXBzzBeZcFcq7RjgSE0JLaRiW3lSD4DveAHlzrOvoTbO1T7vd/S3lYrad4d3pSFj
-        a9RIpevF9MWOyPiwF4MD7tk4dHh//Cu/soSFDJX3c3xICSW09ukZQU3AfNVOa7Lv
-        E6qF25SEaxz5MqSMTUXfafg4CsO0wPEuYnf6Xv6ffh8IWlUTMgvJi9Vmn34OqdJq
-        msTN+g1UgdoVz2B8zAQzAbRaIxgddhkVVW7EqyxRAgMBAAGjUDBOMB0GA1UdDgQW
-        BBQvjS4SHIKbxyolWYlFv6em4rJ3TjAfBgNVHSMEGDAWgBQvjS4SHIKbxyolWYlF
-        v6em4rJ3TjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4ICAQBpiWMKZHa8
-        TW6GZ0BEO5POBPOWSUua8gYlBnlDLP9wUiRC0vOvmQa/EH7SNrCwYxtPOy9EL3Jo
-        igH9gcRGRrQFgvuTV7pE9Bvw0NhcTllnHNGzHld/y93eZSakg+7M4B5Elx0RdRBc
-        e09QvR0P0HGZkVrpdT9P1q6+AAUfbelpSQOBDgUwaZHXWmz3fb2iZ/oCQz13QxKi
-        wdwQueK7090An4y4U9pjVII2b+E7rxQksZXGCRWbeZrEAXkqnuJoBoQQbIYgE2Lo
-        0e9gNR8pvdLy76m9KNYq7v5Agqemwn+5rfYoknd4WePbD4zdndZxDEbdYLpn9eXB
-        oSXiiX1BFgTgeVo+25EI0KRfoaeZvT4TD+04RdKr6XJDH02bs1fqcwv0cpsiAfIR
-        hjmhu8Zcj3kVOo14II2LQblb4FL3ojFZNv9WXe+XPMJx2B6btV6FWXQ/s2yXkfsJ
-        bjMITcD1H6kiZ8oWEMruXxtQPMrnn9p/nuAZpzl970q8oXe03y4sA7EPRodICtAt
-        RDWRi9LvX2vpNkiMrTgm0WA2cTHCfa0JPd2DHlvFQSGNEpV0F3/OAZSp4/aPICnr
-        76XZb0N5TnPYY+fA6mw9ftDHkdiVxCaaPD6mxlj/JUouFJkq+KlquVzA6LNr2rdF
-        9PARtscKmMRcsOLB0ZoSokGbWS0qAazRIA=='),
+        'x509cert' => env('SAML2_IDP_x509', 'MIIDCTCCAfGgAwIBAgIJAJb36sy/0qwIMA0GCSqGSIb3DQEBCwUAMBsxGTAXBgNV
+        BAMMEHNzby5wcC51bml2cG0uaXQwHhcNMTgwOTEwMTMzMDA1WhcNMzgwOTA5MTMz
+        MDA1WjAbMRkwFwYDVQQDDBBzc28ucHAudW5pdnBtLml0MIIBIjANBgkqhkiG9w0B
+        AQEFAAOCAQ8AMIIBCgKCAQEA40prKyVtJj2s598Uz2jJfvw7JT4dQcIYSWTEZm+I
+        VvuaFMaW5vOEsZroR8zchETnP0YFlbaSSeiISA5v9EL+9gOIPi1EukECIhq7RGFx
+        cSUkzzYCCiyiSYJIRi18Y1ic5uyH2ju737mbIfzjA0MJCrfneJ89MQiryUhwMvpl
+        ve9lR594GL9A+krsECJ3jpwPyMwT7O8ktO2IQCacgCw2m2apgmYclIiuVtBsNwZW
+        uBsG5zJqTJ6FUKW7sJDqA3m8ug43W6IAIJ+0vU4xNmnyzluP5QX6gftfE3YUlbfd
+        C+Fe/wUcqVScGDuFOly6wE1/t73cfN0n5RA7/+Np5PFcMwIDAQABo1AwTjAdBgNV
+        HQ4EFgQUNI4DH29gzZehjWDl7WgeXamDqBIwHwYDVR0jBBgwFoAUNI4DH29gzZeh
+        jWDl7WgeXamDqBIwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAdq+a
+        hbcxyQH+iCVrbD6qG0RpnAEj54DfKATx0k+SISoHQHHW7jg+4QhdXtiOjyydmG1d
+        S/WWqcLAdJgATMgjMInwoGQ78LaOczcJAZ75lBxhzUdavBdCxwSTaxZQXcuVKLbw
+        IrbsdJpQvt0zCN8uRJyUCypsT7o41cfCGr0YCVGJx6oVRyppypIMoS2ekxThzzhX
+        4H7+peWEp/JK8G62lUofSW73DLRJPNHwC7zFzzY++9Yx6l4sOWV1KK5Y7dvsl81Q
+        JD8gcC2awRYZlOao/eFOctNOoQpWwgyEnUvX41Sbzv+j4y6D0cHNFHnmdM7I1uh5
+        OouYVCUFUt6+5hx+7g=='),
         /*
          *  Instead of use the whole x509cert you can use a fingerprint
          *  (openssl x509 -noout -fingerprint -in "idp.crt" to generate it)
@@ -169,38 +156,38 @@ return $settings = array(
         // this SP to be encrypted.
         'wantNameIdEncrypted' => false,
 
-        'wantAssertionsEncrypted' => true,
+        'wantAssertionsEncrypted' => false,
         // Authentication context.
         // Set to false and no AuthContext will be sent in the AuthNRequest,
         // Set true or don't present thi parameter and you will get an AuthContext 'exact' 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'
         // Set an array with the possible auth context values: array ('urn:oasis:names:tc:SAML:2.0:ac:classes:Password', 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509'),
-        'requestedAuthnContext' => false,        
+        'requestedAuthnContext' => false,
 
           //'signatureAlgorithm' => 'http://www.w3.org/2000/09/xmldsig#rsa-sha1',
         'signatureAlgorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
-      
+
         //'digestAlgorithm' =>  'http://www.w3.org/2000/09/xmldsig#sha1',
         'digestAlgorithm' => 'http://www.w3.org/2001/04/xmlenc#sha256',
     ),
 
-    // Contact information template, it is recommended to suply a technical and support contacts
-    'contactPerson' => array(
+   // Contact information template, it is recommended to suply a technical and support contacts
+   'contactPerson' => array(
         'technical' => array(
-            'givenName' => 'name',
-            'emailAddress' => 'no@reply.com'
+            'givenName' => env('SUPPORT_NAME', ''),
+            'emailAddress' => env('SUPPORT_EMAIL', '')
         ),
         'support' => array(
-            'givenName' => 'Support',
-            'emailAddress' => 'no@reply.com'
+            'givenName' => env('SUPPORT_NAME', ''),
+            'emailAddress' => env('SUPPORT_EMAIL', '')
         ),
     ),
 
     // Organization information template, the info in en_US lang is recomended, add more if required
     'organization' => array(
-        'en-US' => array(
-            'name' => 'Name',
-            'displayname' => 'Display Name',
-            'url' => 'http://url'
+        'it-IT' => array(
+            'name' => env('ATENEO_NAME', ''),
+            'displayname' => env('ATENEO_DISPLAYNAME', ''),
+            'url' => env('ATENEO_HOME_URL', '')
         ),
     ),
 
