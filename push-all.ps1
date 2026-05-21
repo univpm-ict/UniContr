0522726ea98550cb2f587b5e0a1bc1cec2f8c097
Francesco Antonio Pileo <f.a.pileo@staff.univpm.it>
2026-05-21 09:34:43 +0200

    modificato script

--- push-all.ps1
+++ push-all.ps1
@@ -1,16 +1,20 @@
+$branch = git branch --show-current
+
+Write-Host "Branch corrente: $branch"
+
 Write-Host "Push monorepo su GitHub..."
-git push origin main
+git push origin $branch
 
 Write-Host "Split e push backend su Bitbucket..."
 git branch -D split-backend 2>$null
 git subtree split --prefix=unicontract-backend -b split-backend
-git push bitbucket-be split-backend:main
+git push bitbucket-be split-backend:$branch
 git branch -D split-backend
 
 Write-Host "Split e push frontend su Bitbucket..."
 git branch -D split-frontend 2>$null
 git subtree split --prefix=unicontract-frontend -b split-frontend
-git push bitbucket-fe split-frontend:main
+git push bitbucket-fe split-frontend:$branch
 git branch -D split-frontend
 
 Write-Host "Fatto."
