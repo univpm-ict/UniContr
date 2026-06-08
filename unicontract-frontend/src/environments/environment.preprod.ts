b8043f71231435740abb9573e4e2d5a776bec8cd
Francesco Antonio Pileo <f.a.pileo@staff.univpm.it>
2026-06-08 12:35:10 +0200

    modifiche base-href e cartelle deploy

--- unicontract-frontend/src/environments/environment.preprod.ts
+++ unicontract-frontend/src/environments/environment.preprod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  API_URL: 'https://unicontr.univpm.it/unicontr/unicontr/public/',
-  baseHref: '/unicontr/',
+  API_URL: 'https://unicontr.univpm.it/unicontr-pp/unicontr/public/',
+  baseHref: '/unicontr-pp/',
   whitelistedDomains: ['unicontr.univpm.it'],
-  blacklistedRoutes: ['https://unicontr.univpm.it/unicontr/unicontr/public/auth/'],
-  documentation: 'https://unicontr.univpm.it/unicontr/documentation/'
+  blacklistedRoutes: ['https://unicontr.univpm.it/unicontr-pp/unicontr/public/auth/'],
+  documentation: 'https://unicontr.univpm.it/unicontr-pp/documentation/'
 };
